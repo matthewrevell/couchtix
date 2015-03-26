e5de5f2e7aea2ed6a728be929e5ff5c21c79d9a0
Matthew Revell <matthew@understated.co.uk>
2015-03-26 15:12:46 +0000

    Basic index and layout in Jade

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+// Basic set-up for our Express app
 var express = require('express');
 var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 
 // Define some simple routes
 
 app.get('/', function(req, res){
-  res.send('Ticketyboo');
-})
+  res.render('index.jade', {title: 'Ticketyboo'});
+});
+
+app.get('/admin', function(req, res){
+
+});
 
 
 app.listen(3000, function() {
   console.log('Ticketyboo listening on port 3000');
-})
+});
