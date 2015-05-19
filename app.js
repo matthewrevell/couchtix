50bd61def7acf935215f0620a6ddc50fee81d1c0
Matthew Revell <matthew@understated.co.uk>
2015-05-19 13:13:03 +0100

    Basic user loading

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var urlEncodedParser = bodyParser.urlencoded({ extended: false });
 // Load our models
 var event = require('./models/event.js');
 var tickets = require('./models/ticket.js');
+var user = require('./models/user.js');
 
 // Define our basic routes
 
@@ -31,30 +32,13 @@ app.get('/', function(req, res) {
   event.load(cb, function getTicketIndex(err, eventDetail) {
     tickets.loadIndex(cb, function(err, ticketIndex) {
       tickets.loadEach(cb, ticketIndex, function(err, ticketDetails) {
-        res.render('index', { eventDetails: eventDetail, ticketList: ticketIndex, ticketDetails: ticketDetails });
+        user.load(cb, "matthewrevell", function(err, userDoc) {
+          res.render('index', { eventDetails: eventDetail, ticketList: ticketIndex, ticketDetails: ticketDetails, userDoc: userDoc });
+        });
       });
     });
   });
 });
-
-app.get('/first-run', function(req, res) {
-  // The first time the app is run we want to gather some basic config and admin-user info
-  res.render('first-run');
-});
-
-app.post('/first-run', urlEncodedParser, function(req, res) {
-  if (!req.body) {
-    return res.sendStatus(400);
-  } else {
-    adminUser = new User(req.body.name, req.body.email, cb);
-    console.log('adminUser: ' + adminUser);
-    adminUser.save();
-    console.log(adminUser.email);
-    res.render('first-run-confirm', adminUser);
-
-  }
-});
-
 
 app.listen(3000, function() {
   console.log('Ticketyboo listening on port 3000');
--- models/user.js
+++ models/user.js
@@ -2,52 +2,7 @@
 
 var keyPrefix = "u::";
 
-// Public
-module.exports = User;
-
-//Constructor
-function User(id, name, email, cb, admin) {
-  this.cb = cb;
-  this.id = keyPrefix + id;
-  this.name = name;
-  this.email = email; 
-  this.admin = admin;
-};
-
-//Methods
-User.prototype.load = function(id) {
-  this.id = keyPrefix + id;
-  console.log("Attempting to load " + this.id);
-  cb.get(this.id, function(err, response) {
-    if (err && err.code === 13) {
-      return false;
-    }
-    else {
-      console.log(response);
-    }; 
-  });
-};
-
-User.prototype.save = function() {
-  // For now we'll use the user's email address as the document key.
-  // We'll use an upsert because we'll call this only where we definitely want
-  // to save the user profile.
-  console.log("Attempting to save");
-  if (this.validate()) { 
-    this.cb.upsert(this.email, {name: this.name}, function(err, res) {
-      if (err) {
-        console.log('Failed to save the user: ', err);
-        return err;
-      } else {
-        console.log("Saved user to " + this.email);
-      }
-    });
-  } else {
-    return false;
-  }
-};
-
-User.prototype.validate = function () {
+var validate = function () {
   console.log("Validating the user object");
   
   // Validate the name
@@ -67,3 +22,16 @@ User.prototype.validate = function () {
   }
   
 };
+
+// Public
+
+module.exports.load = function(cb, user, callback) {
+  this.cb = cb;
+  user = "u::" + user;
+  console.log("Loading user: " + user);
+  cb.get(user, function(err, response) {
+    if (err) throw err;
+    console.log(response.value);
+    callback(null, response.value);
+  });
+}
